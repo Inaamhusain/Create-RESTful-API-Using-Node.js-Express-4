2480e067597aea1693925b44abd7901f15399da5
inaam husain <inaam@reach1to1.com>
2015-02-27 12:06:13 +0530

    Adding Files
    Adding Files

---
+++ server.js
@@ -0,0 +1,114 @@
+var app   = require('express')();
+var http = require('http').Server(app);
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'books',
+	});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+	
+app.get('/',function(req,res){
+	var data = {
+		"Data":""
+	};
+	data["Data"] = "Welcome to Book Store DEMO...";
+	res.json(data);
+});
+
+app.get('/books',function(req,res){
+	var data = {
+		"error":1,
+		"Books":""
+	};
+	
+	connection.query("SELECT * from book",function(err, rows, fields){
+		if(rows.length != 0){
+			data["error"] = 0;
+			data["Books"] = rows;
+			res.json(data);
+		}else{
+			data["Books"] = 'No books Found..';
+			res.json(data);
+		}
+	});
+});
+
+app.post('/addbook',function(req,res){
+	var Bookname = req.body.bookname;
+	var Authorname = req.body.authorname;
+	var Price = req.body.price;
+	var data = {
+		"error":1,
+		"Books":""
+	};
+	if(!!Bookname && !!Authorname && !!Price){
+		connection.query("INSERT INTO book VALUES('',?,?,?)",[Bookname,Authorname,Price],function(err, rows, fields){
+			if(!!err){
+				data["Books"] = "Error Adding data";
+			}else{
+				data["error"] = 0;
+				data["Books"] = "Book Added Successfully";
+			}
+			res.json(data);
+		});
+	}else{
+		data["Books"] = "Please provide all required data (i.e : Bookname, Authorname, Price)";
+		res.json(data);
+	}
+});
+
+app.post('/updatebook',function(req,res){
+	var Id = req.body.id;
+	var Bookname = req.body.bookname;
+	var Authorname = req.body.authorname;
+	var Price = req.body.price;
+	var data = {
+		"error":1,
+		"Books":""
+	};
+	if(!!Id && !!Bookname && !!Authorname && !!Price){
+		connection.query("UPDATE book SET BookName=?, AuthorName=?, Price=? WHERE id=?",[Bookname,Authorname,Price,Id],function(err, rows, fields){
+			if(!!err){
+				data["Books"] = "Error Updating data";
+			}else{
+				data["error"] = 0;
+				data["Books"] = "Updated Book Successfully";
+			}
+			res.json(data);
+		});
+	}else{
+		data["Books"] = "Please provide all required data (i.e : id, Bookname, Authorname, Price)";
+		res.json(data);
+	}
+});
+
+app.post('/deletebook',function(req,res){
+	var Id = req.body.id;
+	var data = {
+		"error":1,
+		"Books":""
+	};
+	if(!!Id){
+		connection.query("DELETE FROM book WHERE id=?",[Id],function(err, rows, fields){
+			if(!!err){
+				data["Books"] = "Error deleting data";
+			}else{
+				data["error"] = 0;
+				data["Books"] = "Delete Book Successfully";
+			}
+			res.json(data);
+		});
+	}else{
+		data["Books"] = "Please provide all required data (i.e : id )";
+		res.json(data);
+	}
+});
+
+http.listen(8080,function(){
+	console.log("Connected & Listen to port 8080");
+});
